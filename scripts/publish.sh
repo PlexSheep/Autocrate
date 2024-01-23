eb35527f101eedf6716a3563e66044059cbbafe3
PlexSheep <software@cscherr.de>
2024-01-23 23:43:02 +0100

    set a prealpha version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 set -e
 cargo check --all-features
-echo ">>>>>>>> SELECT A NEW VERSION"
-cargo ws version --no-git-commit
-NEW_VERSION=$(cat Cargo.toml | rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
-bash scripts/set_all_versions.sh $NEW_VERSION
-git add -A
-git commit -m "Release v$NEW_VERSION" || (echo ">>>>>>>> COMMIT FAILED OR THERE WAS NOTHING TO COMMIT"; sleep 5)
-echo ">>>>>>>> SKIP!!!!!"
-cargo ws version --amend
 echo ">>>>>>>> PUBLISHING RELEASE FOR REPO"
 bash scripts/release.sh
 echo ">>>>>>>> PUBLISHING TO CRATES.IO NEXT"
-sleep 10
-cargo publish -p libpt-log
-cargo publish -p libpt-core
-cargo publish -p libpt-bintols
-cargo publish -p libpt-math
-cargo publish -p libpt-net
-cargo publish -p libpt
+sleep 2
+cargo publish
 echo ">>>>>>>> PUBLISHING TO CSCHERR.DE NEXT"
-sleep 3
-cargo publish --registry cscherr -p libpt-log
-cargo publish --registry cscherr -p libpt-core
-cargo publish --registry cscherr -p libpt-bintols
-cargo publish --registry cscherr -p libpt-math
-cargo publish --registry cscherr -p libpt-net
-cargo publish --registry cscherr -p libpt
+sleep 2
+cargo publish --registry cscherr
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 TOKEN=$(cat ~/.git-credentials | grep 'git.cscherr.de' | grep -P '(?:)[^:]*(?=@)' -o)
 NEW_VERSION=$(cat Cargo.toml | rg '^\s*version\s*=\s*"([^"]*)"\s*$' -or '$1')
-VERSION=$(git rev-parse HEAD)
 GIT_COMMIT_SHA=$(git rev-parse HEAD)
+REPO=autocrate
 BODY="
 $(git log $(git describe --tags --abbrev=0)..HEAD --pretty="- %s" --oneline --decorate)
 "
 USER=PlexSheep
 git tag "v$NEW_VERSION-test" || echo "could not tag"
 curl -X 'POST' \
-	'https://git.cscherr.de/api/v1/repos/PlexSheep/pt/releases' \
+	'https://git.cscherr.de/api/v1/repos/PlexSheep/'$REPO'/releases' \
 	-H 'accept: application/json' \
 	-H "Authorization: token $TOKEN" \
 	-H 'Content-Type: application/json' \
